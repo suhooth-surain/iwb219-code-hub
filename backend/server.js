e6426154b4592841bbc0f72bd0fac724470b0aaa
surain5262 <surain5262@gmail.com>
2024-10-20 21:28:11 +0530

    New Project Created

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express, { response } from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+
+// app config 
+const app = express()
+const port = process.env.PORT || 4000
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api endpoint
+
+app.get('/',(req,res)=>{
+    res.send('API WORKING')
+})
+
+app.listen(port, ()=> console.log("Server Started",port))
